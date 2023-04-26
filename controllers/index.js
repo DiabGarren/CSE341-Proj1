809163924140e32e6dac316c8ebb7532580b6d52
DiabGarren <garrendiab@gmail.com>
2023-04-26 14:18:54 +0200

    Added post, put and delete

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,8 @@
+/* eslint-disable no-undef */
 const mongodb = require('../db');
+const objectId = require('mongodb').ObjectId;
 
-const returnPerson = (req, res, next) => {
-    res.json('Maeve Human');
-};
-
-const getData = async (req, res, next) => {
+const getData = async (req, res) => {
     const result = await mongodb.getDb().db().collection('contacts').find();
     result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
@@ -12,7 +10,7 @@ const getData = async (req, res, next) => {
     });
 };
 
-const getContact = async (req, res, next) => {
+const getContact = async (req, res) => {
     const result = await mongodb.getDb().db().collection('contacts').find();
     result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
@@ -20,8 +18,52 @@ const getContact = async (req, res, next) => {
             if (contact._id == req.params.id) {
                 res.status(200).json(contact);
             }
-        })
+        });
     });
 };
 
-module.exports = { returnPerson, getData, getContact };
+const createContact = async (req, res) => {
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+    if (response.acknowledged) {
+        res.status(201).json(response);
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    }
+};
+
+const updateContact = async (req, res) => {
+    const id = new objectId(req.params.id);
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDb().db().collection('contacts').replaceOne({ _id: id }, contact);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    }
+};
+
+const deleteContact = async (req, res) => {
+    const id = new objectId(req.params.id);
+    const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: id });
+    console.log(response);
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    }
+};
+
+module.exports = { getData, getContact, createContact, updateContact, deleteContact };
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-undef */
 const routes = require('express').Router();
 const myController = require('../controllers');
 
 routes.get('/', myController.getData);
 routes.get('/:id', myController.getContact);
+
+routes.post('/', myController.createContact);
+routes.put('/:id', myController.updateContact);
+
+routes.delete('/:id', myController.deleteContact);
 
 module.exports = routes;
