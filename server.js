cb66398667082549d625ce6ead05891c45353dc8
DiabGarren <garrendiab@gmail.com>
2023-04-29 01:58:37 +0200

    Added access to get, post, put and delete

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const port = process.env.PORT || 3000;
 
 
 app
+    // .use(cors())
     .use(bodyParser.json())
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
         next();
     })
     .use('/', require('./routes'));
