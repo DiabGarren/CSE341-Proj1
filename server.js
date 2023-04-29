33ae739f5cdd9de9c4ea57501d85ea2ff969a2f7
DiabGarren <garrendiab@gmail.com>
2023-04-29 02:02:42 +0200

    Added options

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app
     .use(bodyParser.json())
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
         next();
     })
     .use('/', require('./routes'));
